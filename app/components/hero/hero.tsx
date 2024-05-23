fc1cdde176035b66c59c06d23505f491171fd1f4
Vuja <djordjijevujovic210306@gmail.com>
2024-05-23 23:43:33 +0200

    image change

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -31,10 +31,10 @@ export default function Hero({ lang }: any) {
   ];
 
   return (
-    <div className="bg-photo min-w-full  rounded-3xl px-12 max-md:px-3 max-md:flex max-md:flex-col grid grid-cols-3 h-[680px] max-sm:max-h-[600px] max-md:h-space-y-10 max-md:mt-[100px]">
+    <div className="bg-photo min-w-full backdrop-brightness-50 backdrop-brightness-200 backdrop-blur-sm rounded-3xl px-12 max-md:px-3 max-md:flex max-md:flex-col grid grid-cols-3 h-[680px] max-sm:max-h-[600px] max-md:h-space-y-10 max-md:mt-[100px]">
       <div className="py-24 space-y-10 col-span-2 max-md:col-span-3 items-center mt-[120px] ">
         <Typewriter
-          onInit={(typewriter) => {
+                onInit={(typewriter) => {
             typewriter
               .typeString(lang.welcome)
               .callFunction(() => {})
