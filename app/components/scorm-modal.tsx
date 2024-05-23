7dfd469fcd66877c44a6daf35480b82aa7398580
Vuja <djordjijevujovic210306@gmail.com>
2024-05-23 20:29:19 +0200

    cert download

--- app/components/scorm-modal.tsx
+++ app/components/scorm-modal.tsx
@@ -18,6 +18,8 @@ import { Config } from "@/Config";
 import { getCookie } from "cookies-next";
 //@ts-ignore
 import confetti from "canvas-confetti";
+import { DownloadIcon, PlayIcon } from "lucide-react";
+import axios from "axios";
 
 export default function ScormModal({
   data,
@@ -39,7 +41,6 @@ export default function ScormModal({
           particleCount: 1000,
           spread: 200,
         });
-        
       }
     };
 
@@ -49,6 +50,34 @@ export default function ScormModal({
       window.removeEventListener("message", handleIframeMessage);
     };
   }, []);
+    async function download() {
+      try {
+        const response = await axios.get(Config.API_URL+'/download/'+course, {
+            responseType: 'blob',
+            headers: {
+              Authorization:
+                "Bearer "+getCookie("token"),
+            },  
+        });
+  
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+  
+        // Kreiramo link za preuzimanje
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'certificate.pdf'); // Postavljamo naziv datoteke za preuzimanje
+  
+        // Dodajemo link u DOM i simuliramo klik
+        document.body.appendChild(link);
+        link.click();
+  
+        // Čistimo URL i uklanjamo link iz DOM-a
+        window.URL.revokeObjectURL(url);
+        document.body.removeChild(link);
+    } catch (error) {
+        console.log(error);
+    }
+    }
   return (
     <>
       <Button onPress={onOpen} color="primary">
@@ -56,7 +85,7 @@ export default function ScormModal({
       </Button>
 
       <Modal
-      onClose={() => setFinish(false)}
+        onClose={() => setFinish(false)}
         isOpen={isOpen}
         onOpenChange={onOpenChange}
         className="min-w-[80%] h-[90vh] overflow-hidden p-0 my-0"
@@ -67,7 +96,8 @@ export default function ScormModal({
           base: "border-[#292f46] bg-[#19172c] dark:bg-[#19172c] text-[#a8b0d3] py-0 my-0",
           header: "border-b-[1px] border-[#292f46]",
           footer: "border-t-[1px] border-[#292f46]",
-          closeButton: "hover:bg-red-500 transition-all w-[120px] h-[30px]  hover:text-white text-center flex justify-center items-center  text-white bg-red-500 active:bg-white/10 z-30 ",
+          closeButton:
+            "hover:bg-red-500 transition-all w-[120px] h-[30px]  hover:text-white text-center flex justify-center items-center  text-white bg-red-500 active:bg-white/10 z-30 ",
         }}
       >
         <ModalContent className="my-0">
@@ -100,10 +130,19 @@ export default function ScormModal({
               )}
             </>
             {finish && (
-              <button className="absolute w-full h-full z-20  backdrop:opacity-100  backdrop-brightness-50 backdrop-blur-sm  flex justify-center items-center" onClick={() => setFinish(false)}>
+              <button
+                className="absolute w-full h-full z-20  backdrop:opacity-100  backdrop-brightness-50 backdrop-blur-sm  flex flex-col justify-center items-center"
+                
+              >
                 <h1 className="uppercase text-white font-extrabold text-3xl">
-                Congratulations,<br/> you have successfully completed the course
+                  Congratulations,
+                  <br /> you have successfully completed the course
                 </h1>
+                <p>Certificate was sent to your email!</p>
+                <div className="space-x-5 mt-3">
+                  <Button color="primary" onPress={() => download()}>Download certificate<DownloadIcon></DownloadIcon></Button>
+                  <Button color="primary" onPress={() => setFinish(false)}>Resume<PlayIcon></PlayIcon></Button>
+                </div>
               </button>
             )}
           </>
