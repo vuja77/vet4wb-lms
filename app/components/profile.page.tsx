7cd0e03d9ee7aee5b5546ef9943c8ace5ff51a4c
Vuja <djordjijevujovic210306@gmail.com>
2024-05-26 16:10:47 +0200

    profile courses fix

--- app/components/profile.page.tsx
+++ app/components/profile.page.tsx
@@ -61,7 +61,7 @@ export default async function ProfilePage({
               </div>
             </div>
           </div>
-          <div className="grid grid-cols-3 max-sm:w-full justify-items-center gap-10 max-lg:grid-cols-2 max-[600px]:grid-cols-1">
+          <div className="grid grid-cols-2 max-sm:w-full justify-items-center gap-10 max-lg:grid-cols-2 max-[600px]:grid-cols-1">
             {courses &&
               courses.map((e: any, index: number) => {
                 return <CourseCard data={e} key={index} progress></CourseCard>;
