b3661040bc255b3f30fef686948f3d50b41506a4
Vuja <djordjijevujovic210306@gmail.com>
2024-06-14 19:00:19 +0200

    a

--- app/components/nav/nav.tsx
+++ app/components/nav/nav.tsx
@@ -39,7 +39,7 @@ export default function Nav({ lang }: any) {
       <Navbar
         onMenuOpenChange={setIsMenuOpen}
         shouldHideOnScroll
-        className={`fixed top-0 py-2 w-full backdrop-blur-0 bg-transparent`}
+        className={`fixed top-0 py-2 w-full backdrop-blur-0 bg-transparent dark:text-white`}
       >
         <NavbarContent>
           <NavbarMenuToggle
@@ -53,18 +53,18 @@ export default function Nav({ lang }: any) {
           </NavbarBrand>
         </NavbarContent>
 
-        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarContent className="hidden dark:text-white sm:flex gap-4" justify="center">
           <NavbarItem isActive>
-            <Link href="/">{lang.home}</Link>
+            <Link href="/" className="dark:text-white">{lang.home}</Link>
           </NavbarItem>
           {hasCookie("token") && (
             <NavbarItem>
-              <Link href="/dashboard">{lang.dashboard}</Link>
+              <Link href="/dashboard" className="dark:text-white">{lang.dashboard}</Link>
             </NavbarItem>
           )}
 
           <NavbarItem>
-            <Link href="https://vet4wb.com/news/">{lang.activities}</Link>
+            <Link href="https://vet4wb.com/news/" className="dark:text-white">{lang.activities}</Link>
           </NavbarItem>
         </NavbarContent>
         <NavbarContent justify="end">
