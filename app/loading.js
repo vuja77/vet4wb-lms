3e0e77e19b6f9df3adcca89a2452961b61591387
Vuja <djordjijevujovic210306@gmail.com>
2024-06-24 20:53:46 +0200

    a

--- app/loading.js
+++ app/loading.js
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Loading() {
   return <div className="fixed w-full h-full bg-white dark:bg-black flex justify-center z-50 items-center">
-    <Image className="animate-bounce" src={"/logo.png"} alt="logo" width={100} height={100}></Image>
+    <Image className="animate-bounce" src={"/logo.webp"} alt="logo" width={100} height={100}></Image>
   </div>;
 }
