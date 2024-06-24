ee1a773e8ed030de174cc9deca82b009c4d9087f
Vuja <djordjijevujovic210306@gmail.com>
2024-06-24 21:02:20 +0200

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n: {
-    // These are all the locales you want to support in
-    // your application
+    
     locales: ["en-US", "fr", "nl-NL"],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
+  
     defaultLocale: "en-US",
-    // This is a list of locale domains and the default locale they
-    // should handle (these are only required when setting up domain routing)
-    // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
+
   },
+  compress: true,
 };
 
 export default nextConfig;
