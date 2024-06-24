e805f53d676a6c8a6094b9c9411b914dce6a2fed
Vuja <djordjijevujovic210306@gmail.com>
2024-06-24 21:57:13 +0200

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,7 @@
-import withBundleAnalyzer from '@next/bundle-analyzer';
-
-// Define your Next.js configuration object
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-
+ 
   compress: true,
 };
 
-// Create the bundle analyzer configuration
-const bundleAnalyzerConfig = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-// Export the merged configuration
-export default {
-  ...bundleAnalyzerConfig,
-  ...nextConfig,
-};
+export default nextConfig;
