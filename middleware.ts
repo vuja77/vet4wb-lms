22db06283ae298278c86d003ebf1231e834c8015
Vuja <djordjijevujovic210306@gmail.com>
2024-07-06 15:54:54 +0200

    a

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,16 @@
-// middleware.js
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 
-// URL vašeg API-a za provjeru administratorskih privilegija
 const ADMIN_API_URL = 'https://moodle.edu4wb.com/api/details';
 
 export async function middleware(request:NextRequest) {
   const url = request.nextUrl.clone();
   
-  // Uzimanje tokena iz kolačića
   const token = request.cookies.get('token')?.value;
 
-  // Provjerite da li ruta sadrži /admin ili /dashboard
   if (url.pathname.startsWith('/admin') || url.pathname.startsWith('/dashboard')) {
 
     if (!token) {
-      // Ako nema tokena, preusmjerite korisnika na stranicu za login
       return NextResponse.redirect(new URL('/login', request.url));
     }
 
@@ -26,16 +21,12 @@ export async function middleware(request:NextRequest) {
           'Authorization': `Bearer ${token}`
         }
       })).json();
-
-      // const data = await response.json();
       if (response.role_id != 795734325693) {
-        // Ako korisnik nije admin, preusmjerite ga na stranicu za login ili neku drugu stranicu
         return NextResponse.redirect(new URL('/login', request.url));
       }
     }
   }
 
-  // Ako je sve u redu, proslijedite zahtjev dalje
   return NextResponse.next();
 }
 
