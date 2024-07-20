69222d9ae68ca43e5da99c85f89f1cea7472d52e
sretenka1 <sretenkasebek@gmail.com>
2024-07-20 02:44:14 +0200

    a

--- app/(protected)/admin/dashboard/page.tsx
+++ app/(protected)/admin/dashboard/page.tsx
@@ -30,15 +30,15 @@ export default async function Page() {
     disabled: "danger",
     pending: "warning",
   };
-  
+
   const courses = await getMineCourse();
   const users = await countUsers();
   const reports = await getReports();
   const lastusers = await lastUsers();
   return (
-    <main className="w-full h-[100vh] pt-[100px] p-12 grid grid-cols-4 gap-5">
-      <div className="grid min-w-full grid-cols-3 col-span-4  gap-5  h-[220px]">
-        <div className="col-span-1 h-full flex flex-col w-full gap-5">
+    <main className="w-full  min-sh-[100vh]  max-h-[500vh] pt-[100px] max-md:flex flex-col p-12 grid grid-cols-4 max-md:p-3 max-md:pt-24  gap-5">
+      <div className="grid min-w-full grid-cols-3 max-md:grid-cols-1 max-md:flex flex-col col-span-4  gap-5 flex-1  md:min-h-[260px]">
+        <div className="col-span-1 max-md:h-[200px] h-full flex flex-col w-full gap-5">
           <Link className=" flex-1 flex " href="/admin/users">
             <Card className="flex-1 h-full hover:translate-y-[-5px] cursor-pointer ">
               <CardBody className="flex flex-row items-center gap-5">
@@ -66,67 +66,76 @@ export default async function Page() {
             </Card>
           </Link>
         </div>
-        <Card className="col-span-2   cursor-pointer  h-[23 0px]">
-          <p className="px-10 pt-5 text-lg  font-medium">
-            Users per country
-          </p>
+        <Card className="col-span-2 max-md:min-h-[500px] max-md:col-span-1 max-md:row-span-1   cursor-pointer  h-[23 0px]">
+          <p className="px-10 pt-5 text-lg  font-medium">Users per country</p>
           <CardBody className="flex flex-row items-center gap-5 ">
             <CountryChart></CountryChart>
           </CardBody>
         </Card>
       </div>
-      <Card className="col-span-2  row-span-10 h-full flex flex-col p-2">
-        <CardHeader className="flex justify-between"><h1>Latest reports</h1> <Link href="/admin/reports" className="text-xs dark:text-white/50">View all</Link></CardHeader>
+      <Card className="col-span-2 max-md:col-span-4  md:row-span-10 h-full flex flex-col p-2">
+        <CardHeader className="flex justify-between">
+          <h1>Latest reports</h1>{" "}
+          <Link href="/admin/reports" className="text-xs dark:text-white/50">
+            View all
+          </Link>
+        </CardHeader>
         <CardBody className="gap-5">
           {reports.map((report: any) => {
-            return (<>
-              <div className="w-full flex items-center gap-5  ">
-                <User
-                  avatarProps={{
-                    radius: "lg",
-                    src:
-                      report.user.photo &&
-                      `https://moodle.edu4wb.com/storage/${report.user.photo}`,
-                  }}
-                  description={report.user.email}
-                  name={report.user.name} // assuming the user object has a name property
-                />
-                <p className="text-xs line-clamp-2">{report.message}</p>
-              </div>
-              <hr className="w-[90%] self-center dark:border-white/10"></hr>
-</>
+            return (
+              <>
+                <div className="w-full flex items-center gap-5  ">
+                  <User
+                    avatarProps={{
+                      radius: "lg",
+                      src:
+                        report.user.photo &&
+                        `https://moodle.edu4wb.com/storage/${report.user.photo}`,
+                    }}
+                    description={report.user.email}
+                    name={report.user.name} // assuming the user object has a name property
+                  />
+                  <p className="text-xs line-clamp-2">{report.message}</p>
+                </div>
+                <hr className="w-[90%] self-center dark:border-white/10"></hr>
+              </>
             );
           })}
         </CardBody>
       </Card>
-      <Card className="col-span-2  row-span-10 h-full flex flex-col p-2">
-        <CardHeader className="flex justify-between"><h1>Newest Registered Users</h1> <Link href="/admin/users" className="text-xs dark:text-white/50">View all</Link></CardHeader>
+      <Card className="col-span-2 max-md:col-span-4 md:row-span-10  h-full flex flex-col p-2">
+        <CardHeader className="flex justify-between">
+          <h1>Newest Registered Users</h1>{" "}
+          <Link href="/admin/users" className="text-xs dark:text-white/50">
+            View all
+          </Link>
+        </CardHeader>
         <CardBody className="gap-5">
           {lastusers.map((user: any) => {
-            return (<>
-              <div className="w-full flex items-center gap-5  justify-between">
-                <User
-                  avatarProps={{
-                    radius: "lg",
-                    src:
-                      user.photo &&
-                      `https://moodle.edu4wb.com/storage/${user.photo}`,
-                  }}
-                  description={user.email}
-                  name={user.name} // assuming the user object has a name property
-                />
+            return (
+              <>
+                <div className="w-full flex items-center gap-5  justify-between">
+                  <User
+                    avatarProps={{
+                      radius: "lg",
+                      src:
+                        user.photo &&
+                        `https://moodle.edu4wb.com/storage/${user.photo}`,
+                    }}
+                    description={user.email}
+                    name={user.name} // assuming the user object has a name property
+                  />
                   <Chip
-                  className="capitalize cursor-pointer"
-                  color={statusColorMap[user.status]}
-                  size="sm"
-                  variant="flat"
-
-                >
-                  {user.status}
-                </Chip>
-              </div>
-              <hr className="w-[90%] self-center  dark:border-white/10"></hr>
-</>
+                    className="capitalize cursor-pointer"
+                    color={statusColorMap[user.status]}
+                    size="sm"
+                    variant="flat"
+                  >
+                    {user.status}
+                  </Chip>
+                </div>
+                <hr className="w-[90%] self-center  dark:border-white/10"></hr>
+              </>
             );
           })}
         </CardBody>
