82e77ea0af6b6ba530311fdc9e202943fd62f142
sretenka1 <sretenkasebek@gmail.com>
2024-07-26 10:53:58 +0200

    a

--- app/(protected)/my-files/page.tsx
+++ app/(protected)/my-files/page.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Button, Card, CardBody, CardHeader, Chip } from "@nextui-org/react";
+import { Button, Card, CardBody, CardHeader } from "@nextui-org/react";
 import { Divider } from "@nextui-org/react";
-import { getMineCourse, getMineFiles } from "@/app/actions/course";
+import { getMineFiles } from "@/app/actions/course";
 import { getLang } from "@/utils/lang";
 import AddFiles from "@/app/components/add-files";
-import { Download } from "lucide-react";
 import AddFilesS from "@/app/components/add-files-s";
 export default async function App() {
   const files = await getMineFiles();
@@ -14,7 +13,7 @@ export default async function App() {
       <div className="w-full">
         <div className="space-y-1 flex justify-between">
           <div className="space-y-1">
-            <h4 className="text-3xl font-medium">Files-D</h4>
+            <h4 className="text-3xl font-medium">My Files</h4>
           </div>
           <AddFiles></AddFiles>
         </div>
@@ -22,17 +21,17 @@ export default async function App() {
       </div>
       <div className=" max-sm:w-full justify-items-center gap-5 max-lg:grid-cols-2 max-[600px]:grid-cols-1 min-w-full">
         <Card className="max-md:col-span-4 min-w-full flex-1 h-full flex flex-col p-2">
-          <CardHeader className="flex justify-between dark:bg-white/10 grid grid-cols-4 bg-black/10  rounded-lg ">
-            <p className="col-span-1 text-center w-1/2 text-xs">
+          <CardHeader className=" dark:bg-white/10 grid grid-cols-3 bg-black/10  rounded-lg ">
+            <p className="col-span-1 text-left w-1/2 text-xs">
               Number of deliverables (as in application)
             </p>
-            <p className="col-span-1 text-center w-1/2 text-xs">
+            <p className="col-span-1 text-center  text-xs">
               Title of deliverable (as in application)
             </p>
-            <p className="col-span-1 w-1/2 text-center text-xs">
+            {/* <p className="col-span-1 w-1/2 text-center text-xs">
               File- Documents
-            </p>
-            <p className="text-right w-1/2 text-xs">actions</p>
+            </p> */}
+            <p className="text-right text-xs w-full">actions</p>
           </CardHeader>
           <CardBody className="gap-5">
             {files
@@ -40,11 +39,13 @@ export default async function App() {
               .map((file: any, index: number) => {
                 return (
                   <>
-                    <div className="w-full   items-center gap-12 grid grid-cols-6  justify-between">
-                      <p>{`${file.type}${index + 1}`}</p>
-                      <p className=" col-span-2  ">{file.description}</p>
-
-                      <p className="col-span-2">{file.display_name}</p>
+                    <div className="w-full   items-center gap-12 grid grid-cols-3  justify-between">
+                      <p>{`${file.type}.${index + 1}.`}</p>
+                      <p className=" col-span-1  text-left">
+                        {file.description}
+                      </p>
+                      {/* 
+                      <p className="col-span-2">{file.display_name}</p> */}
                       <div className="text-end flex justify-end items-center">
                         <a
                           target="_blank"
@@ -85,7 +86,8 @@ export default async function App() {
       <div className="w-full mt-12">
         <div className="space-y-1 flex justify-between">
           <div className="space-y-1">
-            <h4 className="text-3xl font-medium">Files - S</h4>
+            {/* <h4 className="text-3xl font-medium">Files - S</h4> */}
+            <br />
           </div>
           <AddFilesS></AddFilesS>
         </div>
@@ -94,13 +96,13 @@ export default async function App() {
       <div className=" max-sm:w-full justify-items-center gap-5 max-lg:grid-cols-2 max-[600px]:grid-cols-1 min-w-full">
         <Card className="max-md:col-span-4 min-w-full flex-1 h-full flex flex-col p-2">
           <CardHeader className="flex justify-between dark:bg-white/10 grid grid-cols-3 bg-black/10  rounded-lg ">
-            <p className="col-span-1 text-center w-1/2 text-xs">
+            <p className="col-span-1 text-left w-1/2 text-xs">
               Number of supporting document
             </p>
-            <p className="col-span-1 text-center w-1/2 text-xs">
+            <p className="col-span-1 text-center  text-xs">
               Title of supporting document
             </p>
-            <p className="text-right w-1/2 text-xs">actions</p>
+            <p className="text-right w-1/2 text-xs w-full">actions</p>
           </CardHeader>
           <CardBody className="gap-5">
             {files
@@ -109,7 +111,7 @@ export default async function App() {
                 return (
                   <>
                     <div className="w-full   items-center gap-12 grid grid-cols-3  justify-between">
-                      <p>{`${file.type}${index + 1}`}</p>
+                      <p>{`${file.type}.${index + 1}.`}</p>
                       <p className=" col-span-1  ">{file.description}</p>
                       <div className="text-end flex justify-end items-center">
                         <a
